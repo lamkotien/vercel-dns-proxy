31d366f9d4aa3e64c93052aa6cd5d091198b72cb
lamkotien <lamkotien000@gmail.com>
2025-12-26 13:45:48 +0700

    dns.js

---
+++ api/dns.js
@@ -0,0 +1,27 @@
+export default async function handler(req) {
+  const upstream = "https://xxx.cloudflare-gateway.com/dns-query"; // THAY LINK CỦA BẠN
+  const ecsSubnet = "14.161.0.0"; // IP VNPT HCM (hoặc IP của bạn)
+
+  const url = new URL(req.url);
+  const searchParams = url.searchParams;
+  const method = req.method;
+
+  let fetchUrl = `${upstream}${url.search}`;
+
+  const headers = {
+    'Accept': 'application/dns-message',
+    'X-Forwarded-For': ecsSubnet
+  };
+
+  let options = { method, headers };
+
+  if (method === 'POST') {
+    options.body = await req.arrayBuffer();
+    headers['Content-Type'] = 'application/dns-message';
+  }
+
+  const response = await fetch(fetchUrl, options);
+  return new Response(await response.arrayBuffer(), {
+    headers: { 'Content-Type': 'application/dns-message' }
+  });
+}
